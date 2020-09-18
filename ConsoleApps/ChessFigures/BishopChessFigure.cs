730c1945f94ce99e0eac8c7fee79aee20488623b
invakid404 <emi.boneva@abv.bg>
2020-09-18 18:42:34 +0300

    Added bishop figure

---
+++ ConsoleApps/ChessFigures/BishopChessFigure.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ChessFigures
+{
+    internal class BishopChessFigure : ChessFigure
+    {
+        public override bool Attacks(Tuple<int, int> selfPos, Tuple<int, int> targetPos)
+        {
+            var deltaX = Math.Abs(targetPos.Item1 - selfPos.Item1);
+            var deltaY = Math.Abs(targetPos.Item2 - selfPos.Item2);
+
+            return deltaX == deltaY;
+        }
+    }
+}
