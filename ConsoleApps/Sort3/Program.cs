58f0db8f344fb3ef4eac63a81393853a259d129a
invakid404 <emi.boneva@abv.bg>
2020-09-18 21:04:44 +0300

    Added Sort3 solution

---
+++ ConsoleApps/Sort3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Sort3
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadInt(out var a);
+            ReadInt(out var b);
+            ReadInt(out var c);
+
+            if (a > b) Swap(ref a, ref b);
+            if (b > c) Swap(ref b, ref c);
+            if (a > b) Swap(ref a, ref b);
+            
+            Console.WriteLine($@"{a} {b} {c}");
+        }
+
+        private static void ReadInt(out int x)
+        {
+            while (!int.TryParse(Console.ReadLine(), out x))
+            {
+            }
+        }
+
+        private static void Swap<T>(ref T x, ref T y)
+        {
+            var tmp = x;
+            x = y;
+            y = tmp;
+        }
+    }
+}
