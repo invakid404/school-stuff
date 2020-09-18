932ac5c9fc1ed85369a1eee5548d03d908514466
invakid404 <emi.boneva@abv.bg>
2020-09-18 21:04:31 +0300

    Added Max3 solution

---
+++ ConsoleApps/Max3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Max3
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadInt(out var a);
+            ReadInt(out var b);
+            ReadInt(out var c);
+
+            Console.WriteLine(Math.Max(a, Math.Max(b, c)));
+        }
+
+        private static void ReadInt(out int x)
+        {
+            while (!int.TryParse(Console.ReadLine(), out x))
+            {
+            }
+        }
+    }
+}
