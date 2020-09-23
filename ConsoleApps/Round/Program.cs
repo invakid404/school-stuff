6cac3ba05a239d4bf5a8f5fc357f8356f1ac95a8
invakid404 <emi.boneva@abv.bg>
2020-09-23 11:18:20 +0300

    Added Round solution

---
+++ ConsoleApps/Round/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Round
+{
+    static internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadDouble(out var y);
+
+            var x = Math.Round(y);
+            // x = Math.Floor(y + 0.5);
+            Console.WriteLine(x);
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
