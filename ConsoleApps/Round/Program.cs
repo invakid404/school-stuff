812a5b0710bd2e124967af7929e0b08e1296dcf1
invakid404 <emi.boneva@abv.bg>
2020-09-23 11:20:00 +0300

    Minor refactoring

--- ConsoleApps/Round/Program.cs
+++ ConsoleApps/Round/Program.cs
@@ -8,7 +8,7 @@ namespace Round
 {
     static internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             ReadDouble(out var y);
 
