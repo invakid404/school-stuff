cee15ca03cfaa59684cfcd589fbd6cfe3af42e29
invakid404 <emi.boneva@abv.bg>
2020-09-23 11:08:24 +0300

    Added Painting Walls solution

---
+++ ConsoleApps/PaintingWalls/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PaintingWalls
+{
+    static internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var height);
+            ReadDouble(out var width);
+            ReadDouble(out var paintArea);
+
+            var paintBoxAmt = Math.Ceiling((height * width) / paintArea);
+            Console.WriteLine($@"Amount of paint boxes: {paintBoxAmt}");
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
