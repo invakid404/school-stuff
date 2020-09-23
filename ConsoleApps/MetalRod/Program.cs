8b531ff8b2d8fb1f52d55927d0121c22b69f4f05
invakid404 <emi.boneva@abv.bg>
2020-09-23 21:54:01 +0300

    Added Metal Rod solution

---
+++ ConsoleApps/MetalRod/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MetalRod
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var rodLen);
+            ReadDouble(out var partLen);
+
+            var partAmt = Math.Floor(rodLen / partLen);
+            Console.WriteLine($@"You can cut into {partAmt} parts.");
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
