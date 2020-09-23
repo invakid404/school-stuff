5d5c2da0b04c858db025b2053df8e4c4482200fa
invakid404 <emi.boneva@abv.bg>
2020-09-23 10:53:05 +0300

    Added Circle solution

---
+++ ConsoleApps/Circle/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Circle
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var r);
+
+            var s = Math.PI * r * r;
+            Console.WriteLine($@"Area: ${s}");
+
+            var d = 2 * Math.PI * r;
+            Console.WriteLine($@"Circumference: ${d}");
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
