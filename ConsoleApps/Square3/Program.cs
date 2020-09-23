563002665a7ff6197ecb17843e2b235365a20e23
invakid404 <emi.boneva@abv.bg>
2020-09-23 21:55:57 +0300

    Added Square3 solution

---
+++ ConsoleApps/Square3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Square3
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var area);
+
+            var side = Math.Sqrt(area);
+            var perimeter = 4 * side;
+
+            Console.WriteLine($@"Perimeter = {perimeter}");
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
