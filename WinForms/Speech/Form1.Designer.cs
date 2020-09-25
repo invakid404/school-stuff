148f9c1d779c3d12746843e6d7d7069e549f4452
invakid404 <emi.boneva@abv.bg>
2020-09-25 09:16:08 +0300

    Added Speech

---
+++ WinForms/Speech/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace KIUR
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnHello = new System.Windows.Forms.Button();
+            this.btnGoodbye = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnHello
+            // 
+            this.btnHello.Location = new System.Drawing.Point(12, 31);
+            this.btnHello.Name = "btnHello";
+            this.btnHello.Size = new System.Drawing.Size(75, 23);
+            this.btnHello.TabIndex = 0;
+            this.btnHello.Text = "Hello";
+            this.btnHello.UseVisualStyleBackColor = true;
+            this.btnHello.Click += new System.EventHandler(this.btnHello_Click);
+            // 
+            // btnGoodbye
+            // 
+            this.btnGoodbye.Location = new System.Drawing.Point(713, 31);
+            this.btnGoodbye.Name = "btnGoodbye";
+            this.btnGoodbye.Size = new System.Drawing.Size(75, 23);
+            this.btnGoodbye.TabIndex = 1;
+            this.btnGoodbye.Text = "Goodbye";
+            this.btnGoodbye.UseVisualStyleBackColor = true;
+            this.btnGoodbye.Click += new System.EventHandler(this.btnGoodbye_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(12, 75);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(776, 363);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnGoodbye);
+            this.Controls.Add(this.btnHello);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnHello;
+        private System.Windows.Forms.Button btnGoodbye;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ WinForms/Speech/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.CodeDom;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Windows.Forms;
+
+namespace KIUR
+{
+    public partial class Form1 : Form
+    {
+        private readonly SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            var msg = "Hello my friend!";
+
+            this.synthesizer.SpeakAsync(msg);
+            MessageBox.Show(msg);
+        }
+
+        private void btnGoodbye_Click(object sender, EventArgs e)
+        {
+            this.synthesizer.Speak("Goodbye my friend!");
+
+            Application.Exit();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            var word = "AWR";
+            var amt = 100;
+
+            var awr = new StringBuilder(amt * word.Length);
+            for (var i = 0; i < amt; ++i)
+            {
+                awr.Append(word);
+            }
+
+            this.synthesizer.Speak(awr.ToString());
+        }
+    }
+}
