c5302629d5a8f945223845d40330ff19c1839528
invakid404 <emi.boneva@abv.bg>
2020-09-29 23:29:32 +0300

    Added RealEq solution

---
+++ ConsoleApps/RealEq/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace RealEq
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        { 
+            ReadDouble(out var x);
+            
+            var y = x <= 5 ? 2 * x - 3 : (x * x + 1) / x;
+            Console.WriteLine(y);
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
