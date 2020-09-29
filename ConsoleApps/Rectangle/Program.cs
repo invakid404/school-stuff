44e03ed24c614e8dd729daaaaeeda4a922d242db
invakid404 <emi.boneva@abv.bg>
2020-09-29 23:03:30 +0300

    Added Rectangle solution

---
+++ ConsoleApps/Rectangle/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Rectangle
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadInt(out var w);
+            ReadInt(out var h);
+
+            var perimeter = 2 * (w + h);
+            var area = w * h;
+            Console.WriteLine($@"Perimeter: {perimeter}, Area: {area}.");
+        }
+
+        private static void ReadInt(out int v)
+        {
+            while (!int.TryParse(Console.ReadLine(), out v))
+            {
+            }
+        }
+    }
+}
