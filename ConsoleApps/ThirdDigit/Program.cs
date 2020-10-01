586a5b48a42912de18829a0b00270ab6c5279b26
invakid404 <emi.boneva@abv.bg>
2020-10-01 22:43:58 +0300

    Added ThirdDigit

---
+++ ConsoleApps/ThirdDigit/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ThirdDigit
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+
+            Console.WriteLine($@"Third digit {(n / 100 % 10 == 7 ? "is" : "is not")} equal to 7");
+        }
+    }
+}
