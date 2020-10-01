e52f6b1ba36b6a7ea18cb5f6278c9517d3597da2
invakid404 <emi.boneva@abv.bg>
2020-10-01 22:35:35 +0300

    Added Even or Odd

---
+++ ConsoleApps/EvenOrOdd/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EvenOrOdd
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+
+            Console.Write($@"{n} is {(n % 2 == 0 ? "even" : "odd")}");
+        }
+    }
+}
