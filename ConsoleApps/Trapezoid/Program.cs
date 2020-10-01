0e4f24424bd35f0e34b2133afbabcab54cdc6764
invakid404 <emi.boneva@abv.bg>
2020-10-01 22:47:22 +0300

    Added Trapezoid

---
+++ ConsoleApps/Trapezoid/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Trapezoid
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble("a", out var a);
+            ReadDouble("b", out var b);
+            ReadDouble("h", out var h);
+
+            Console.WriteLine($@"Area: {(a + b) / 2 * h:0.##}");
+        }
+
+        private static void ReadDouble(string msg, out double d)
+        {
+            do
+            {
+                Console.Write($@"{msg} = ");
+            } while (!double.TryParse(Console.ReadLine(), out d));
+        }
+    }
+}
