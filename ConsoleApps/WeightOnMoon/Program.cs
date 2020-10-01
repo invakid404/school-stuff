506c157918023eb8281774370875857d4cdd5542
invakid404 <emi.boneva@abv.bg>
2020-10-01 22:49:25 +0300

    Added Weight on Moon

---
+++ ConsoleApps/WeightOnMoon/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace WeightOnMoon
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            double weight;
+            while (!double.TryParse(Console.ReadLine(), out weight))
+            {
+            }
+            
+            Console.WriteLine($@"Weight on moon: {weight * 0.17:0.##} kg");
+        }
+    }
+}
