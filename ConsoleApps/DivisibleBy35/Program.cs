18a33630e41f1ec68e3b8302b79bb1c50cc2c34b
invakid404 <emi.boneva@abv.bg>
2020-10-01 22:40:16 +0300

    Added Divisible by 35

---
+++ ConsoleApps/DivisibleBy35/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DivisibleBy35
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+            
+            Console.WriteLine($@"{n} {(n % 35 == 0 ? "is" : "is not")} divisible by 5 and 7.");
+        }
+    }
+}
