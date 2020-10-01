cff9e2f2b1d025dd30914ea332473b50f27371eb
invakid404 <emi.boneva@abv.bg>
2020-10-01 22:52:47 +0300

    Added InCircle

---
+++ ConsoleApps/InCircle/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace InCircle
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var x);
+            ReadDouble(out var y);
+            
+            Console.WriteLine($@"The point ({x}, {y}) {(x * x + y * y <= 25 ? "is" : "is not")} in the circle.");
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
