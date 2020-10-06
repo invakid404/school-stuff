7ddaf9ec16665d9a0ea21703bcceac485875c0cb
invakid404 <emi.boneva@abv.bg>
2020-10-06 22:01:55 +0300

    Added Contains7 solution

---
+++ ConsoleApps/Contains7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Contains7
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+            }
+
+            a = Math.Abs(a);
+            
+            var b = false;
+            while (a != 0)
+            {
+                var d = a % 10;
+                if (d == 7)
+                {
+                    b = true;
+                    break;
+                }
+                
+                a /= 10;
+            }
+            
+            Console.WriteLine(b);
+        }
+    }
+}
