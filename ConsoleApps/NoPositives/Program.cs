6dd802c851721eb17929409fc57c0f9aa7193c22
invakid404 <emi.boneva@abv.bg>
2020-10-06 21:50:57 +0300

    Added NoPositives solution

---
+++ ConsoleApps/NoPositives/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace NoPositives
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadInt(out var a);
+            ReadInt(out var b);
+            ReadInt(out var c);
+            
+            Console.WriteLine(!(a > 0 || b > 0 || c > 0));
+        }
+
+        private static void ReadInt(out int v)
+        {
+            while (!int.TryParse(Console.ReadLine(), out v))
+            {
+            }
+        }
+    }
+}
