6305c49d07b8a82640e68c39ee5ec4ff6668cc17
invakid404 <emi.boneva@abv.bg>
2020-10-06 22:06:51 +0300

    Added DifferentDigits solution

---
+++ ConsoleApps/DifferentDigits/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace DifferentDigits
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+            }
+
+            a = Math.Abs(a);
+
+            var b = true;
+            var digits = new bool[10];
+            while (a != 0)
+            {
+                var d = a % 10;
+                if (digits[d])
+                {
+                    b = false;
+                    break;
+                }
+
+                digits[d] = true;
+                a /= 10;
+            }
+            
+            Console.WriteLine(b);
+        }
+    }
+}
