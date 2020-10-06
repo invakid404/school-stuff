0dec73019d9a76a063b6bd6d76635a4d38934491
invakid404 <emi.boneva@abv.bg>
2020-10-06 22:09:57 +0300

    Added NotDifferentDigits solution

---
+++ ConsoleApps/NotDifferentDigits/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace NotDifferentDigits
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a;
+            while (!int.TryParse(Console.ReadLine(), out a))
+            {
+            }
+
+            a = Math.Abs(a);
+
+            var b = false;
+            var digits = new bool[10];
+            while (a != 0)
+            {
+                var d = a % 10;
+                if (digits[d])
+                {
+                    b = true;
+                    break;
+                }
+
+                digits[d] = true;
+                a /= 10;
+            }
+            
+            Console.WriteLine(b);
+        }
+    }
+}
