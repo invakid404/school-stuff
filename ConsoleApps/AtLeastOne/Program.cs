72d12ae8aa82a7a1e82842f50176e89222788a99
invakid404 <emi.boneva@abv.bg>
2020-10-06 21:41:17 +0300

    Added AtLeastOne solution

---
+++ ConsoleApps/AtLeastOne/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AtLeastOne
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadBoolean(out var a);
+            ReadBoolean(out var b);
+            
+            Console.WriteLine(a || b);
+        }
+
+        private static void ReadBoolean(out bool v)
+        {
+            while (!bool.TryParse(Console.ReadLine(), out v))
+            {
+            }
+        }
+    }
+}
