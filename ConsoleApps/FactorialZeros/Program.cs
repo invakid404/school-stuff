28251021146a335c263d08805583e3bd7388fc39
invakid404 <emi.boneva@abv.bg>
2020-10-07 10:58:30 +0300

    Added FactorialZeros solution

---
+++ ConsoleApps/FactorialZeros/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FactorialZeros
+{
+    static internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+
+            var res = 0;
+            while (n > 0)
+            {
+                n /= 5;
+                res += n;
+            }
+
+            Console.WriteLine(res);
+        }
+    }
+}
