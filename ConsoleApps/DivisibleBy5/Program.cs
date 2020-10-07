97cd4b6415fddf4015a158ff8c8c6c1aff8398ff
invakid404 <emi.boneva@abv.bg>
2020-10-07 10:58:14 +0300

    Added DivisibleBy5 solution

---
+++ ConsoleApps/DivisibleBy5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace DivisibleBy5
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+
+            var b = n % 5 == 0;
+            Console.WriteLine(b);
+        }
+    }
+}
