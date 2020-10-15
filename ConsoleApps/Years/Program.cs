04a542211a2ba30a19e9cbb67696b43ae20a5581
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:40:13 +0300

    Years

---
+++ ConsoleApps/Years/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Years
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int years;
+            while (!int.TryParse(Console.ReadLine(), out years))
+            {
+            }
+
+            var days = years * 365;
+            var hours = days * 24;
+            var minutes = hours * 60;
+            
+            Console.WriteLine($@"Days: {days}, hours: {hours}, minutes: {minutes}");
+        }
+    }
+}
