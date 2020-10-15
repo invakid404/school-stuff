db2c200ead76ab7482c1f1cf9d7d8aaa431020c0
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:26:29 +0300

    Added FahrenheitToCelsius solution

---
+++ ConsoleApps/FahrenheitToCelsius/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FahrenheitToCelsius
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            double fahrenheitT;
+            while (!double.TryParse(Console.ReadLine(), out fahrenheitT))
+            {
+            }
+
+            var celsiusT = (fahrenheitT - 32) * 5 / 9;
+            Console.WriteLine($@"Temperature in Celsius: {celsiusT:0.##}");
+        }
+    }
+}
