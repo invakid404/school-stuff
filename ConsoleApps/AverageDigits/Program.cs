ae6d9acdb5e598989fd951af38fe26eac178698b
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:49:09 +0300

    Added AverageDigits solution

---
+++ ConsoleApps/AverageDigits/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AverageDigits
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+
+            var res = (n % 10 + n / 10 % 10) / 2.0;
+            Console.WriteLine(res);
+        }
+    }
+}
