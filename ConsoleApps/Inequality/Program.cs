152cf5e5512c166a1d096de493719c2e3759c627
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:28:34 +0300

    Added Inequality solution

---
+++ ConsoleApps/Inequality/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Inequality
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var x);
+            ReadDouble(out var a);
+            
+            Console.WriteLine(x * x + 5 * a <= 18);
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
