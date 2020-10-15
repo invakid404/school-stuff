e521a583782770bd0f876897f2af93aac6deeb27
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:56:07 +0300

    Added Equal3 solution

---
+++ ConsoleApps/Equal3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Equal3
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var a);
+            ReadDouble(out var b);
+            ReadDouble(out var c);
+
+            Console.WriteLine(AreEqual(a, b) && AreEqual(b, c));
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+
+        private static bool AreEqual(double x, double y)
+        {
+            return Math.Abs(x - y) < double.Epsilon;
+        }
+    }
+}
