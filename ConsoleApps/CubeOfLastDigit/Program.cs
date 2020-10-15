beed702f8aa06d93b1c92aad82467599227928c3
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:54:02 +0300

    Added CubeOfLastDigit solution

---
+++ ConsoleApps/CubeOfLastDigit/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CubeOfLastDigit
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+
+            var res = (int) Math.Pow(n % 10, 3);
+            Console.WriteLine(res);
+        }
+    }
+}
