d7fb36ef9aa488266cd1954eadd0cbe3ebd74628
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:44:51 +0300

    Added Fraction2 solution

---
+++ ConsoleApps/Fraction2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fraction2
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            double x;
+            while (!double.TryParse(Console.ReadLine(), out x))
+            {
+            }
+
+            var res = (Math.Sqrt(6 * Math.Abs(x)) + 3) / (Math.Pow(x, 4) + 2);
+            Console.WriteLine(res);
+        }
+    }
+}
