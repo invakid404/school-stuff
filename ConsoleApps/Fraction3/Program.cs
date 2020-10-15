fd7c645da308042666f40d71904884278040029f
invakid404 <emi.boneva@abv.bg>
2020-10-15 23:00:08 +0300

    Added Fraction3 solution

---
+++ ConsoleApps/Fraction3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Fraction3
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var m);
+            ReadDouble(out var n);
+
+            var res = 45 - Math.Abs(m + n) * Math.Sqrt(Math.Pow(m, 3) - 1) / (6 + 2 * n);
+            Console.WriteLine(res);
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
