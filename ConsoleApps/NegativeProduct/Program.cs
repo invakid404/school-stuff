d4eca138d2d3828d99741b9a44e3bfbd5cb779bf
invakid404 <emi.boneva@abv.bg>
2020-10-15 23:04:04 +0300

    Added NegativeProduct solution

---
+++ ConsoleApps/NegativeProduct/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NegativeProduct
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var x);
+            ReadDouble(out var y);
+            
+            Console.WriteLine(x < 0 ^ y < 0);
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
