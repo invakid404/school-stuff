ce1c48047d9d0cfd983cb29d4cda0686b0eb96f6
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:37:47 +0300

    Added Equality solution

---
+++ ConsoleApps/Equality/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Equality
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var k);
+            ReadDouble(out var p);
+            
+            Console.WriteLine(Math.Abs(k % 7 - (p / 5 - 2)) < double.Epsilon);
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
