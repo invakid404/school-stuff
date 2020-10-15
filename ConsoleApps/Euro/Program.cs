b3542f425cb158a469ca366c41a63373ea3b9125
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:33:49 +0300

    Added Euro solution

---
+++ ConsoleApps/Euro/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Euro
+{
+    internal static class Program
+    {
+        private const decimal EuroChange = 0.511292M;
+
+        public static void Main(string[] args)
+        {
+            decimal priceLeva;
+            while (!decimal.TryParse(Console.ReadLine(), out priceLeva))
+            {
+            }
+
+            Console.WriteLine($@"Price in euro: {priceLeva * EuroChange:0.00}");
+        }
+    }
+}
