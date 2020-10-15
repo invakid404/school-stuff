ff1a4960e9f35c711d48a59d26b78f937999a5bb
invakid404 <emi.boneva@abv.bg>
2020-10-15 23:01:50 +0300

    Added InInterval solution

---
+++ ConsoleApps/InInterval/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InInterval
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            double d;
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+            
+            Console.WriteLine(2 <= d && d <= 6);
+        }
+    }
+}
