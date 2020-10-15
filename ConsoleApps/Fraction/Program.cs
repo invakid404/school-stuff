df676257ace04bed06a927131d9b6e4179172c49
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:42:24 +0300

    Added Fraction solution

---
+++ ConsoleApps/Fraction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Fraction
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            double m;
+            while (!double.TryParse(Console.ReadLine(), out m))
+            {
+            }
+
+            var res = (m * m + 2 * m - 1) / (5 + Math.Sqrt(Math.Abs(m - 3)));
+            Console.WriteLine($@"{res:0.###}");
+        }
+    }
+}
