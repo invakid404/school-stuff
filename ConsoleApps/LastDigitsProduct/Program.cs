719787b1245d1ae2ecaeef6c407821776661a6ad
invakid404 <emi.boneva@abv.bg>
2020-10-15 23:06:07 +0300

    Added LastDigitsProduct solution

---
+++ ConsoleApps/LastDigitsProduct/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LastDigitsProduct
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n;
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+
+            var res = n % 10 * (n / 10 % 10);
+            Console.WriteLine(res);
+        }
+    }
+}
