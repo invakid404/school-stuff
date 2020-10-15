5749029166bcc6b62a4466870312eb0fcda44997
invakid404 <emi.boneva@abv.bg>
2020-10-15 22:51:01 +0300

    Added AtLeastOnePositive solution

---
+++ ConsoleApps/AtLeastOnePositive/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace AtLeastOnePositive
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadDouble(out var x);
+            ReadDouble(out var y);
+            ReadDouble(out var z);
+            ReadDouble(out var t);
+            
+            Console.WriteLine(x > 0 || y > 0 || z > 0 || t > 0);
+        }
+
+        private static void ReadDouble(out double d)
+        {
+            while (!double.TryParse(Console.ReadLine(), out d))
+            {
+            }
+        }
+    }
+}
