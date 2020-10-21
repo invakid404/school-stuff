748b301bcad5edbf5c6fc04d44dbe5a7efc6579d
invakid404 <emi.boneva@abv.bg>
2020-10-21 10:42:18 +0300

    Added Range solution

---
+++ ConsoleApps/Range/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ReadInt(out var a);
+            ReadInt(out var b);
+
+            for (var i = a; i <= b; ++i)
+            {
+                Console.Write($@"{i} ");
+            }
+            Console.WriteLine();
+        }
+
+        private static void ReadInt(out int n)
+        {
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+        }
+    }
+}
