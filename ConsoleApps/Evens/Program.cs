66a49e015f0e5c75fdbd89193759842f09f4ecd9
invakid404 <emi.boneva@abv.bg>
2020-10-21 10:41:51 +0300

    Added Evens solution

---
+++ ConsoleApps/Evens/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Evens
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (var i = 2; i <= 20; i += 2)
+            {
+                Console.Write($@"{i} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
