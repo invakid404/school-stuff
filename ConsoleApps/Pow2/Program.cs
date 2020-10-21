83804a5f54af48284dceede4ac4c6a7ba535245a
invakid404 <emi.boneva@abv.bg>
2020-10-21 10:42:07 +0300

    Added Pow2 solution

---
+++ ConsoleApps/Pow2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Pow2
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var n = 1D;
+            for (var i = 0; i < 50; i++)
+            {
+                Console.WriteLine(n);
+                n *= 2;
+            }
+        }
+    }
+}
