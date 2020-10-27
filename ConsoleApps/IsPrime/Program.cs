fc80a571678fc0b6cc56838cee78414ba697fe93
invakid404 <emi.boneva@abv.bg>
2020-10-27 09:33:56 +0200

    Added IsPrime solution

---
+++ ConsoleApps/IsPrime/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IsPrime
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            ReadInt(out var n);
+
+            var res = true;
+            var hi = (int) Math.Sqrt(n);
+
+            for (var i = 2; i <= hi; ++i)
+            {
+                if (n % i != 0) continue;
+
+                res = false;
+                break;
+            }
+
+            Console.WriteLine($@"{n} {(res ? "is" : "isn't")} prime");
+        }
+
+        private static void ReadInt(out int n)
+        {
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+        }
+    }
+}
