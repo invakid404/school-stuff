d37e15c153d2bd2d61e4385467a6818ac092de93
invakid404 <emi.boneva@abv.bg>
2020-10-27 09:46:19 +0200

    Added DigitsN solution

---
+++ ConsoleApps/DigitsN/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace DigitsN
+{
+    internal static class Program
+    {
+        public static void Main()
+        {
+            ReadInt(out var n);
+
+            var res = 0;
+            while (n != 0)
+            {
+                n /= 10;
+                ++res;
+            }
+            
+            Console.WriteLine(res);
+        }
+
+        private static void ReadInt(out int n)
+        {
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+        }
+    }
+}
