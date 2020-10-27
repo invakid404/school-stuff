f4739302a921177ea8604ee2b63624329b669c5b
invakid404 <emi.boneva@abv.bg>
2020-10-27 09:09:24 +0200

    Added Euclid solution

---
+++ WinForms/Euclid/Form1.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Euclid
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSolve = new System.Windows.Forms.Button();
+            this.txtRes = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(3, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "A =";
+            // 
+            // btnSolve
+            // 
+            this.btnSolve.BackgroundImage = global::Euclid.Properties.Resources.download;
+            this.btnSolve.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnSolve.Location = new System.Drawing.Point(148, 12);
+            this.btnSolve.Name = "btnSolve";
+            this.btnSolve.Size = new System.Drawing.Size(200, 200);
+            this.btnSolve.TabIndex = 0;
+            this.btnSolve.UseVisualStyleBackColor = true;
+            this.btnSolve.Click += new System.EventHandler(this.btnSolve_Click);
+            // 
+            // txtRes
+            // 
+            this.txtRes.Location = new System.Drawing.Point(354, 9);
+            this.txtRes.Name = "txtRes";
+            this.txtRes.Size = new System.Drawing.Size(100, 20);
+            this.txtRes.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "B =";
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(42, 9);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(100, 20);
+            this.txtA.TabIndex = 6;
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(42, 35);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(100, 20);
+            this.txtB.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 231);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtRes);
+            this.Controls.Add(this.btnSolve);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnSolve;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtRes;
+
+        #endregion
+    }
+}
---
+++ WinForms/Euclid/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Euclid
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSolve_Click(object sender, EventArgs e)
+        {
+            var a = int.Parse(txtA.Text);
+            var b = int.Parse(txtB.Text);
+
+            txtRes.Text = GreatestCommonDivisor(a, b).ToString();
+        }
+
+        private static int GreatestCommonDivisor(int a, int b)
+        {
+            return b == 0 ? a : GreatestCommonDivisor(b, a % b);
+        }
+    }
+}
