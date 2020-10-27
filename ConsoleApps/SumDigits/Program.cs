d298227de1d2fa7cc2d89602866ada6c580c4eb8
invakid404 <emi.boneva@abv.bg>
2020-10-27 09:49:04 +0200

    Added SumDigits solution

---
+++ ConsoleApps/SumDigits/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace SumDigits
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            ReadInt(out var n);
+            n = Math.Abs(n);
+            
+            var res = 0;
+            while (n != 0)
+            {
+                res += n % 10;
+                n /= 10;
+            }
+            
+            Console.WriteLine(res);
+        }
+
+        private static void ReadInt(out int n)
+        {
+            while (!int.TryParse(Console.ReadLine(), out n))
+            {
+            }
+        }
+    }
+}
