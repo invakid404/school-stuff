79792ba6a024e534f88404d6e4195c5eb3cf1d8a
invakid404 <emi.boneva@abv.bg>
2020-10-28 10:32:53 +0200

    Added Divisors solution

---
+++ WinForms/Divisors/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Divisors
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNum = new System.Windows.Forms.TextBox();
+            this.lstRes = new System.Windows.Forms.ListBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtNum
+            // 
+            this.txtNum.Location = new System.Drawing.Point(12, 12);
+            this.txtNum.Name = "txtNum";
+            this.txtNum.Size = new System.Drawing.Size(109, 20);
+            this.txtNum.TabIndex = 0;
+            // 
+            // lstRes
+            // 
+            this.lstRes.FormattingEnabled = true;
+            this.lstRes.Location = new System.Drawing.Point(12, 46);
+            this.lstRes.Name = "lstRes";
+            this.lstRes.Size = new System.Drawing.Size(109, 147);
+            this.lstRes.TabIndex = 1;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Location = new System.Drawing.Point(136, 9);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(75, 23);
+            this.btnCalc.TabIndex = 2;
+            this.btnCalc.Text = "Calculate";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(136, 46);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 3;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 209);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.lstRes);
+            this.Controls.Add(this.txtNum);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.ListBox lstRes;
+        private System.Windows.Forms.TextBox txtNum;
+
+        #endregion
+    }
+}
---
+++ WinForms/Divisors/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Divisors
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            Clear();
+            var n = int.Parse(txtNum.Text);
+
+            var st = new Stack<int>();
+            
+            var hi = (int) Math.Sqrt(n);
+            for (var i = 1; i <= hi; ++i)
+            {
+                if (n % i != 0) continue;
+                
+                lstRes.Items.Add(i.ToString());
+
+                var other = n / i;
+                if (i != other)
+                {
+                    st.Push(n / i);
+                }
+            }
+
+            while (st.Count > 0)
+            {
+                lstRes.Items.Add(st.Pop().ToString());
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void Clear()
+        {
+            lstRes.Items.Clear();
+        }
+    }
+}
